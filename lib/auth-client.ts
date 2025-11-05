4654921842d33cb8e0d4f8a986e32e21f20b1607
Koneum <148181966+Koneum@users.noreply.github.com>
2025-11-05 04:47:44 +0000

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,10 @@ import { nextCookies } from 'better-auth/next-js'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  plugins: [nextCookies()],
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'https://sissansissan.vercel.app',
+  baseURL: process.env.NODE_ENV === 'production' 
+      ? 'https://eduwaly.vercel.app' 
+      : 'http://localhost:3000',
+    basePath: '/api/auth',
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
